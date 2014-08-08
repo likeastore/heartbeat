e7292255f7c814dba38899398be1eee6eb9c0205
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-08-08 12:37:29 +0300

    mongo strategy

--- config/index.js
+++ config/index.js
@@ -20,54 +20,28 @@ module.exports = {
 		// 	}
 		// ],
 
-		json: [
-			{
-				url: 'https://app.likeastore.com/api/monitor',
-				response: {
-					"app":"app.likeastore.com",
-					"env":"production",
-					"version":"0.0.52",
-					"apiUrl":"/api"
-				}
-			}
-		],
-
-		// html: [
-
-		// ],
-
-		// db: [
-
-		// ]
-
-
-		// // simple site ping, for web apps
-		// site: {
-		// 	url: ['https://likeastore.com', 'https://app.likeastore.com'] // array, string
-		// },
-
-		// // api calls with response check
-		// app: {
-		// 	url: 'http://app.likeastore.com/api/monitor',
-		// 	response: {
-		// 		app: "app.likeastore.com",
-		// 		env: "production",
-		// 		version: "0.0.51",
-		// 		apiUrl: "/api"
-		// 	}
-		// },
-
-		// // databases
-		// db: {
-		// 	mongo: 'mongodb://user:pass@christian.mongohq.com:212443/likeastoreproddb',
-		// 	query: {
-		// 		users: {
-		// 			findOne: {
-		// 				id: '4c5e8d9494fd0f47518dce45'
-		// 			}
+		// json: [
+		// 	{
+		// 		url: 'https://app.likeastore.com/api/monitor',
+		// 		response: {
+		// 			"app":"app.likeastore.com",
+		// 			"env":"production",
+		// 			"version":"0.0.52",
+		// 			"apiUrl":"/api"
 		// 		}
 		// 	}
-		// }
+		// ],
+
+		mongo: [
+			{
+				connection: 'mongodb://localhost:27017/likeastoredb',
+				collections: ['users'],
+				query: function (db, callback) {
+					db.users.findOne({email: 'alexander.beletsky@gmail.com'}, callback);
+				}
+			}
+		]
+
 	},
 
 	// notification options
